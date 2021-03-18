842bdac99d3e358db36bbe79751d7765812d1871
Rodrigo <cuitlagua@gmail.com>
2021-03-17 22:39:44 -0600

    creating mock heroes and import

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -10,6 +11,8 @@ export class HeroesComponent implements OnInit {
     id: 1,
     name: 'Windstorm'
   };
+
+  heroes = HEROES;
 
   constructor() { }
   
