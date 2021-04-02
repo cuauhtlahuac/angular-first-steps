248fc370fd9b08529e77afd6065a1493835e80b6
Rodrigo <cuitlagua@gmail.com>
2021-04-01 22:39:20 -0600

    get error in hero-detail possible null Object

--- angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,17 +1,31 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { HeroService } from '../hero.service';
 import { Hero } from '../hero';
 
 @Component({
-  selector: 'app-hero-detail',
-  templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.scss']
+	selector: 'app-hero-detail',
+	templateUrl: './hero-detail.component.html',
+	styleUrls: [ './hero-detail.component.scss' ],
 })
 export class HeroDetailComponent implements OnInit {
-  @Input() hero?: Hero
+	@Input() hero?: Hero;
 
-  constructor() { }
+	constructor(
+		private route: ActivatedRoute,
+		private heroService: HeroService,
+		private location: Location,
+	) {}
 
-  ngOnInit(): void {
+	ngOnInit(): void {
+    this.getHero();
   }
-
+  
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
 }
