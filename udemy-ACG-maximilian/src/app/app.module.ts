9c2422ed1ac3969bedb2c3c262c7df65a18752c0
Rodrigo <cuitlagua@gmail.com>
2021-04-13 11:19:54 -0500

    new project and creating structure

---
+++ udemy-ACG-maximilian/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipieListComponent } from './recipes/recipie-list/recipie-list.component';
+import { RecipieDetailComponent } from './recipes/recipie-detail/recipie-detail.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipieListComponent,
+    RecipieDetailComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
