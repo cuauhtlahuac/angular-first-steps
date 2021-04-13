f824efcbb818e32f28ea92f4876c48de4149f4e1
Rodrigo <cuitlagua@gmail.com>
2021-04-13 14:05:34 -0500

    creating recipe list structure

--- udemy-ACG-maximilian/src/app/recipes/recipie-list/recipie-list.component.ts
+++ udemy-ACG-maximilian/src/app/recipes/recipie-list/recipie-list.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
-  selector: 'app-recipie-list',
-  templateUrl: './recipie-list.component.html',
-  styleUrls: ['./recipie-list.component.css']
+	selector: 'app-recipie-list',
+	templateUrl: './recipie-list.component.html',
+	styleUrls: [ './recipie-list.component.css' ],
 })
 export class RecipieListComponent implements OnInit {
+	recipes: Recipe[] = [
+		new Recipe(
+			'a Test Recipe',
+			'This is simply text',
+			'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg',
+		),
+	];
 
-  constructor() { }
+	onCreateRecipe(name, desc, urlImg) {
+		this.recipes.push(
+			new Recipe(
+				name,
+				desc,
+				'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg',
+			),
+		);
+	}
 
-  ngOnInit(): void {
-  }
+	constructor() {}
 
+	ngOnInit(): void {}
 }
