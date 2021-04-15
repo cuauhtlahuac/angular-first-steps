6ced38cd3a3f1fdf513247226162bbf08ff129ad
Rodrigo <cuitlagua@gmail.com>
2021-04-15 14:55:55 -0500

    Show recipes my solution : propagation, ngIf

--- udemy-ACG-maximilian/src/app/header/header.component.ts
+++ udemy-ACG-maximilian/src/app/header/header.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 @Component({
 	selector: 'app-header',
 	templateUrl: './header.component.html',
 })
 export class HeaderComponent {
-	collapsed:boolean = true;
+	collapsed: boolean = true;
+	showRecipes: boolean = true;
+	@Output() showRecipesView = new EventEmitter<boolean>();
+
+	onRecipesClick() {
+		this.showRecipes = true;
+		this.showRecipesView.emit(this.showRecipes);
+	}
+
+	onShoppingClick() {
+		this.showRecipes = false;
+		this.showRecipesView.emit(this.showRecipes);
+	}
 }
