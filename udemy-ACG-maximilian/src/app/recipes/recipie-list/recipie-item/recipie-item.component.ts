73c7e586e81310e53072ccb8fa26dcf266c68dac
Rodrigo <cuitlagua@gmail.com>
2021-04-15 15:28:59 -0500

    Binding custom property to recipe item

--- udemy-ACG-maximilian/src/app/recipes/recipie-list/recipie-item/recipie-item.component.ts
+++ udemy-ACG-maximilian/src/app/recipes/recipie-list/recipie-item/recipie-item.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-recipie-item',
+  selector: 'app-recipe-item',
   templateUrl: './recipie-item.component.html',
   styleUrls: ['./recipie-item.component.css']
 })
 export class RecipieItemComponent implements OnInit {
-
+  @Input() recipe: { name: String, desc: String, imagePath: String };
   constructor() { }
 
   ngOnInit(): void {
