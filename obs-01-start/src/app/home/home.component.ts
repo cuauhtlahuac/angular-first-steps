a98cf9b85fd14fb587f97782dbba2c6fa69a5420
Rodrigo <cuitlagua@gmail.com>
2021-04-28 15:10:43 -0500

    Obs: First observable created

--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,14 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    interval(1000) // This is a function provided for rxjs library
+                  // Any second a new event will be emitted
+    .subscribe(
+      count => // Values that are emitted, in this case came from the interval function
+      {
+        console.log({count})
+      }
+    )
   }
 
 }
