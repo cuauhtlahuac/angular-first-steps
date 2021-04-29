b0f1b794420dea64ed7f9ea50bbb5aa5e999af0b
Rodrigo <cuitlagua@gmail.com>
2021-04-29 12:56:49 -0500

    obs: handle errors

--- MaxCourseNotes/Observables.md
+++ MaxCourseNotes/Observables.md
@@ -65,3 +65,32 @@ ngOnInit() {
 		console.log({data});
 	});		 
 ```
+
+### Handle errors
+
+```ts
+	ngOnInit() {
+		const customIntervalObservable = Observable.create((
+			observer, // is the listener
+		) => {
+			let count = 0;
+			setInterval(() => {
+				if (count < 3) {
+					observer.next(count); // To emit a new value
+				} else {
+					observer.error(new Error('time is up')); // Here we provoked and error
+				}
+				count++;
+			}, 1000);
+		});
+
+		customIntervalObservable.subscribe(
+			data => { // receiving the data emitting
+				console.log({ data });
+			},
+			error => { // the second argument is something that should append if there is and error
+				alert('Time is up');
+			},
+		);
+	}
+```
--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -7,33 +7,36 @@ import { Observable, Subscription, throwError } from 'rxjs';
 	styleUrls: [ './home.component.css' ],
 })
 export class HomeComponent implements OnInit, OnDestroy {
-
-  private firstSubscription: Subscription; // here we declare a new Subscription instance
+	private firstSubscription: Subscription; // here we declare a new Subscription instance
 
 	constructor() {}
 
 	ngOnInit() {
-	const customIntervalObservable =	Observable.create(
-		observer => // is the listener
-		  {
-			 let count = 0;
-			 setInterval( () => {
-				 if(count < 3) {
-					 observer.next(count); // To emit a new value
-				 } else {
-					 observer.error(new Error('time is up'))
-				 }
-				 count++
-			 }, 1000)
-		 })
-		
-	customIntervalObservable.subscribe(data => { // receiving the data emitting
-		console.log({data});
-	});		 
+		const customIntervalObservable = Observable.create((
+			observer, // is the listener
+		) => {
+			let count = 0;
+			setInterval(() => {
+				if (count < 3) {
+					observer.next(count); // To emit a new value
+				} else {
+					observer.error(new Error('time is up'));
+				}
+				count++;
+			}, 1000);
+		});
 
+		customIntervalObservable.subscribe(
+			data => { // receiving the data emitting
+				console.log({ data });
+			},
+			error => { // the second argument is something that should append if there is and error
+				alert('Time is up');
+			},
+		);
 	}
 
 	ngOnDestroy(): void {
-		this.firstSubscription.unsubscribe(); // Here we unsubscribe 
+		this.firstSubscription.unsubscribe(); // Here we unsubscribe
 	}
 }
