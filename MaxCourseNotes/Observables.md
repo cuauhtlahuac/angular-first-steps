b1402beec15467f6ff626cf6d24d8f4ccbdf622c
Rodrigo <cuitlagua@gmail.com>
2021-04-29 11:41:31 -0500

    Obs: Using a observable

--- MaxCourseNotes/Observables.md
+++ MaxCourseNotes/Observables.md
@@ -9,3 +9,38 @@
 - We have a stream of events (time line) emitted by the observable.
 - Observable emits
 - Observer handle Data, Errors or Completion
+
+### Using a Observable
+
+- Here we use an observable
+
+```ts
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
+
+@Component({
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: [ './home.component.css' ],
+})
+export class HomeComponent implements OnInit, OnDestroy {
+
+  private firstSubscription: Subscription; // here we declare a new Subscription instance
+
+	constructor() {}
+
+	ngOnInit() {
+		this.firstSubscription = interval(1000) // Here we store the subscription
+			.subscribe((
+				count, // Values that are emitted, in this case came from the interval function
+			) => {
+				console.log({ count });
+			});
+	}
+
+	ngOnDestroy(): void {
+		this.firstSubscription.unsubscribe(); // Here we unsubscribe 
+	}
+}
+
+```
--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -1,24 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { interval } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: [ './home.component.css' ],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private firstSubscription: Subscription; // here we declare a new Subscription instance
 
-  ngOnInit() {
-    interval(1000) // This is a function provided for rxjs library
-                  // Any second a new event will be emitted
-    .subscribe(
-      count => // Values that are emitted, in this case came from the interval function
-      {
-        console.log({count})
-      }
-    )
-  }
+	constructor() {}
 
+	ngOnInit() {
+		this.firstSubscription = interval(1000) // Now we can store the subscription
+			.subscribe((
+				count, // Values that are emitted, in this case came from the interval function
+			) => {
+				console.log({ count });
+			});
+	}
+
+	ngOnDestroy(): void {
+		this.firstSubscription.unsubscribe(); // Here we unsubscribe 
+	}
 }
