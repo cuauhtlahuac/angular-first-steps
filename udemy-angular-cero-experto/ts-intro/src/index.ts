e30a73efbfd3d2977448b0949fa1a48802ce0d3c
Rodrigo <cuitlagua@gmail.com>
2021-05-02 21:14:27 -0500

    TS: Array with multiple values

--- udemy-angular-cero-experto/ts-intro/src/index.ts
+++ udemy-angular-cero-experto/ts-intro/src/index.ts
@@ -1,8 +1,1 @@
-let myName: string = 'Strinder';
-let hp: number | string = 95;
-let estaVivo: boolean = true;
-
-hp = 'Empty'
-estaVivo = false;
-
-console.log(myName, hp, estaVivo)
+let habilidades:( boolean | string | number)[] = ['bash', 'Counter', 'Header', 100, true];
