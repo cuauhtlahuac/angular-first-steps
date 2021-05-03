5e1fa65d7ed913186ba815f4e5cac2a147b872f1
Rodrigo <cuitlagua@gmail.com>
2021-05-03 12:20:09 -0500

    TS: Defining methods to an object

--- udemy-angular-cero-experto/ts-intro/src/index.ts
+++ udemy-angular-cero-experto/ts-intro/src/index.ts
@@ -1,27 +1,34 @@
-function sumar(
-	a: number, // primer parametro con tipo de dato
-	b: number // segundo parámetro especificado
-): number // tipo de dato que será retornado
-{
-	return a + b;
-}
-function multiplicar(
-	// El orden de argumentos es primero obligatorios luego opcionales
-	numero: number,
-	otroNumero?: number, // Este es opcional pero para que no truene al siguiente argumento le damos valor default
-	base: number = 2, //  Valor opcional deben ir al último
-): number
-{
-	return numero * base * otroNumero;
+interface Personaje {
+	nombre: string;
+	pv: number;
+	muestraHP: () => void;
+	apellido: string;
+	fullname?: string;
 }
 
-const resultado = sumar(5, 6);
-const mResultado = 
-multiplicar(
-	5, // Toma el argumento numero
-	9, // Toma argumento otroNumero
-	10, // Asigna valor de 10 para no usar el default 2
-)
+function curar(
+	personaje: Personaje,
+	curarX,
+): void { // Con void indicamos que no retorna nada, aunque JS retorna undefine
+	personaje.pv += curarX;
+}
 
-console.log(resultado);
-console.log(mResultado);
+const nuevoPersonaje: Personaje = {
+	nombre: 'Batman',
+	apellido: 'unknown',
+	pv: 50,
+	muestraHP(): void {
+		console.log('Puntos de vida: ', this.pv);
+	},
+	get fullname() {
+		return this.apellido ? `${this.nombre} ${this.apellido}` : `${this.nombre}`
+	},
+	set fullname(value) {
+		[this.nombre, this.apellido] = value.split(" ");
+	},
+};
+
+curar(nuevoPersonaje, 20);
+
+nuevoPersonaje['fullname'] = 'Bruno Díaz'
+console.log(nuevoPersonaje);
