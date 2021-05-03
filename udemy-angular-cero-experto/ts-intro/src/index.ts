766790e748d05cf50b0af0aac1b019b6cd686ed9
Rodrigo <cuitlagua@gmail.com>
2021-05-02 21:21:40 -0500

    TS: interfases

--- udemy-angular-cero-experto/ts-intro/src/index.ts
+++ udemy-angular-cero-experto/ts-intro/src/index.ts
@@ -1,1 +1,17 @@
-let habilidades:( boolean | string | number)[] = ['bash', 'Counter', 'Header', 100, true];
+interface Personaje {
+	// Es un forma de indicar como queremos que un objeto luzca
+	nombre: String;
+	hp: Number;
+	habilidades: String[];
+	puebloNatal?: String; // El parentesis le dice que TS que es opcional
+}
+
+const personaje: Personaje = {
+	nombre: 'Strider',
+	hp: 100,
+	habilidades: [ 'bash', 'Counter', 'Header' ],
+};
+
+personaje.puebloNatal = 'Zacatlán de las manzanas';
+
+console.table(personaje)
